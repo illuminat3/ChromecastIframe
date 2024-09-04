c76f0ad1ed4bb9eebcff9cfffe161221433754a0
MatthewRawson <mrawson@redev.co.uk>
2024-09-04 14:17:09 +0100

    Swap dto

---
+++ ChromecastIframe/Dtos/WebMessage.cs
@@ -0,0 +1,11 @@
+﻿using Sharpcaster.Messages;
+using System.Runtime.Serialization;
+
+namespace ChromecastIframe.Dtos;
+
+[DataContract]
+public class WebMessage : MessageWithSession
+{
+    [DataMember(Name = "url")]
+    public string Url { get; set; }
+}
