6b017691647031433fa14fc46915d0f797fd8961
MatthewRawson <mrawson@redev.co.uk>
2024-09-04 12:17:17 +0100

    Added Logger Helper

---
+++ ChromecastIframe/Helpers/LoggerHelper.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace ChromecastIframe.Helpers;
+
+public static class LoggerHelper
+{
+    public static ILogger<T> SetupLogger<T>()
+    {
+        var loggerFactory = LoggerFactory.Create(builder =>
+        {
+            builder.AddConsole();
+            builder.SetMinimumLevel(LogLevel.Debug);
+        });
+
+        var logger = loggerFactory.CreateLogger<T>();
+
+        return logger;
+    }
+}
